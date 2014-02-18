e1b3858a62beb2108733ad0d9baad727ee0a64bb
sdlu1992 <sdlu1992@gmail.com>
2014-02-18 22:17:13 +0800

    first commit

---
+++ mycpu.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:08:43 02/18/2014 
+// Design Name: 
+// Module Name:    mycpu 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mycpu(
+    
+    );
+
+
+endmodule
